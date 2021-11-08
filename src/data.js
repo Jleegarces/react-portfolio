f7f696353845146a2ddd4ac6d98aa9fb795e9258
Jleegarces <86203137+Jleegarces@users.noreply.github.com>
2021-11-08 01:06:05 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: "Weather API",
+    subtitle: "API",
+    description:
+      "Weather generator page ",
+    image: "./weather_api.png",
+    link: "https://github.com/Jleegarces/weather-API.git",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "HTML & CSS",
+    description:
+      "minimalistic portfolio page using basic HTML & CSS",
+    image: "./portfolio_img.png",
+    link: "https://jleegarces.github.io/Portfolio/",
+  },
+
+];
+
+
+
